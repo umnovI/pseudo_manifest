7ce7f436c3353c2d118837011e2c448918be55c2
Ilia <30232564+umnovI@users.noreply.github.com>
2024-03-15 04:39:53 +0300

    Refactor exe name generation

--- src/main.rs
+++ src/main.rs
@@ -120,17 +120,19 @@ fn main() -> Result<()> {
         })?
     };
     let release_hash = try_digest(&release_exe)?;
-    let release_url = &release_exe
+    let release_url = &release_exe.to_str().unwrap().replace(r#"\\?\"#, ""); // Scoop can't parse URL otherwise.
+    let bin_name = release_exe
+        .file_name()
+        .unwrap()
         .to_str()
         .unwrap()
-        .to_owned()
-        .replace(r#"\\?\"#, ""); // Scoop can't parse URL otherwise.
+        .to_owned();
 
     let manifest = Manifest {
         version: cargo_meta.version,
         url: release_url.to_owned(),
         hash: release_hash.to_owned(),
-        bin: json!([[&release_exe.file_name().unwrap().to_str(), args.alias]]),
+        bin: json!([[bin_name, args.alias]]),
         license: cargo_meta.license,
         architecture: json!({
             "64bit": {
